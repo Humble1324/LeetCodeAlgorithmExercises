43653228dfa27932a9af3b5fa51e92c6d97c9030
Humble <799544199@qq.com>
2023-07-08 14:15:56 +0800

    daily update

--- LeetCode75/RecentCounter.cs
+++ LeetCode75/RecentCounter.cs
@@ -2,40 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace LeetCode;
 
 public class RecentCounter
 {
     private Queue<int> tQueue = new();
-    public RecentCounter() {
+
+    public RecentCounter()
+    {
         tQueue.Clear();
     }
-    
+
     public int Ping(int t)
     {
         tQueue.Enqueue(t);
-        while(tQueue.Count>0&&tQueue.Peek()> t-3000){
-   
-                tQueue.Dequeue();
-            
+        while (tQueue.Count > 0 && tQueue.Peek() > t - 3000)
+        {
+            tQueue.Dequeue();
         }
+
         return tQueue.Count;
     }
-    
+
     public static int FindPoisonedDuration(int[] timeSeries, int duration)
     {
-        int[] time = new int[timeSeries.Max()+duration];
+        int[] time = new int[timeSeries.Max() + duration];
         int lens = timeSeries.Length;
         for (int i = 0; i < lens; i++)
         {
             int temp = duration;
             while (temp > 0)
             {
-                time[timeSeries[i]+temp-1] = 1;
+                time[timeSeries[i] + temp - 1] = 1;
                 temp--;
             }
-
         }
 
         int sum = 0;
@@ -43,11 +45,57 @@ public class RecentCounter
         {
             if (bigtime == 1) sum++;
         }
+
         return sum;
+    }
+    public string PredictPartyVictory(string senate) {
+        int lens = senate.Length;
+        StringBuilder sb = new StringBuilder(senate,lens);
+        if(lens<2){
+            return sb[0]=='D'? "Dire":"Radiant";
+        }
+        int flag = 0;
+        bool D = false,R=false;
+        while(D&&R){
+            R = false;
+            D = false;
+            for(int i =0;i<lens;i++){
+                if(sb[i]=='D')
+                {
+                    if (flag < 0) sb[i] = ' ';
+                    else R = true;
+                    flag++;
+                }    
+                if(sb[i]=='R'){
+                    if(flag<0)sb[i]=' ';
+                    else D = true;
+                    flag--;
+                }
+                
+            }
+
+        }
+        return R ==true? "Radiant":"Dire";
+    }
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int temp = 0;
+        for (int i = m; i < nums1.Length; i++)
+        {
+            nums1[i] = nums2[temp++];
+        }
+
+        Array.Sort(nums1);
+        return;
+    }
+
+    public void MergeII(int[] nums1, int m, int[] nums2, int n)
+    {
         
     }
+
     public static void Main()
     {
-        Console.Write(FindPoisonedDuration(new int[]{2,4,6,8,10},2));
+        Console.Write(FindPoisonedDuration(new int[] { 2, 4, 6, 8, 10 }, 2));
     }
 }
---
+++ LeetCode75/Tree.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode;
+
+public partial class LeetCode75
+{
+    
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        List<int> tree1 = new List<int>();
+        List<int> tree2 = new List<int>();
+
+        if (root1 != null)
+        {
+            Leaf(root1,tree1);
+        }
+
+        if (root2 != null)
+        {
+            Leaf(root2,tree2);
+        }
+        return tree1.SequenceEqual(tree2);
+    }
+
+    public void Leaf(TreeNode root, List<int> DFSS)
+    {
+        if (root.left == null && root.right == null)
+        {
+            DFSS.Add(root.val);
+        }
+
+        else if (root.left != null)
+        {
+            Leaf(root.left,DFSS);
+        }
+
+        else if (root.right != null)
+        {
+            Leaf(root.right,DFSS);
+        }
+    }
+    public int GoodNodes(TreeNode root)
+    {
+        int max = root.val;
+        List<int> maxLeafs = new List<int>();
+        MaxLeaf(root, max, maxLeafs);
+        return maxLeafs.Count;
+    }
+
+    public void MaxLeaf(TreeNode root, int max, List<int> MaxLeafs)
+    {
+        if (root.val <= max)
+        {
+            MaxLeafs.Add(root.val);
+            max = root.val;
+        }
+        if (root.left != null)
+        {
+            MaxLeaf(root.left, max, MaxLeafs);
+        }
+        if (root.right != null)
+        {
+            MaxLeaf(root.right, max, MaxLeafs);
+        }
+    }
+
+}
+public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
