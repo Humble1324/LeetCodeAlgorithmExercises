32c356f06e5b67047289bc07989cbe4ad4ab04b2
Humble <799544199@qq.com>
2023-07-11 19:17:31 +0800

    daily update

--- LeetCode75/LeetCode.cs
+++ LeetCode75/LeetCode.cs
@@ -124,6 +124,18 @@ namespace LeetCode
 
             return count == n;
         }
+        public int ClimbStairs(int n)
+        {
+            if (n == 1) return 1;
+            int[] dp = new int[n+1];
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = dp[i - 2] + dp[i-1];
+            }
+            return dp[n];
+        }
 
         // public static void Main(string[] args)
         // {
