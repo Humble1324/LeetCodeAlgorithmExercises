d8769f14eeafbc0fcd95026edaece8c5c04037ce
Humble <799544199@qq.com>
2023-08-09 13:25:37 +0800

    daily update

---
+++ DailyCode/SubtractProductAndSum.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.DailyCode;
+
+public partial class DailyCode
+{
+    public int SubtractProductAndSum(int n)
+    {
+        int product = 1;
+        int sum = 0;
+        while (n>0)
+        {
+            int temp = n % 10;
+            product *= temp;
+            sum += temp;
+            n /= 10;
+        }
+        return product-sum;
+    }
+}
