9ba43d697cb5de02931ea4e49ad5bdabbc3585d1
humble1324 <799544199@qq.com>
2024-10-24 10:42:24 +0800

    daily update

--- DailyCode/Y202410.cs
+++ DailyCode/Y202410.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace LeetCode.DailyCode;
 
@@ -81,6 +83,7 @@ public class Y202410
             {
                 break;
             }
+
             if (blue >= tempB)
             {
                 blue -= tempB;
@@ -105,7 +108,11 @@ public class Y202410
                 blue -= tempB;
                 tempB += 2;
                 tempCountB++;
-            }else{break;}
+            }
+            else
+            {
+                break;
+            }
 
             if (red >= tempA)
             {
@@ -120,5 +127,153 @@ public class Y202410
         }
 
         return Math.Max(tempCount, tempCountB);
+    }
+
+    public double MinimumAverage(int[] nums)
+    {
+        double ans = int.MaxValue;
+        Array.Sort(nums);
+        int lens = nums.Length, fast = lens - 1, slow = 0;
+        while (fast > slow)
+        {
+            double temp = (nums[slow] + nums[fast]) / 2.0f;
+            ans = Math.Min(temp, ans);
+            slow++;
+            fast--;
+        }
+
+        return ans;
+    }
+
+    public int MinOperations(int[] nums)
+    {
+        int ans = 0;
+        int lens = nums.Length;
+        for (var i = 0; i < lens; i++)
+        {
+            if (nums[i] == 0 && i < lens - 2)
+            {
+                nums[i] = nums[i] == 0 ? 1 : 0;
+                nums[i + 1] = nums[i + 1] == 0 ? 1 : 0;
+                nums[i + 2] = nums[i + 2] == 0 ? 1 : 0;
+                ans++;
+            }
+        }
+
+        if (nums.Any(t => t == 0))
+        {
+            return -1;
+        }
+
+        return ans > 0 ? ans : -1;
+    }
+
+    public int SmallestRangeI(int[] nums, int k)
+    {
+        int ans = 0;
+        var min = nums.Min();
+        var max = nums.Max();
+        ans = (max - min) - 2 * k;
+        return ans >= 0 ? ans : 0;
+    }
+
+    public int SmallestRangeII(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        int min = nums[0], max = nums[nums.Length - 1], ans = max - min;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int a = nums[i];
+            int b = nums[i + 1];
+            ans = Math.Min(ans, Math.Max(max - k, a + k) - Math.Min(min + k, b - k));
+        }
+
+        return ans;
+    }
+
+    public static long CountCompleteDayPairs(int[] hours)
+    {
+        long ans = 0, lens = hours.Length;
+        int[] cnt = new int[24];
+        foreach (var hour in hours)
+        {
+            ans += cnt[(24 - hour % 24) % 24];
+            cnt[hour % 24]++;
+        }
+
+        return ans;
+    }
+
+    public static int FindWinningPlayer(int[] skills, int k)
+    {
+        int maxIndex = 0, win = 0, lens = skills.Length;
+        for (int i = 1; i < lens && win < k; i++)
+        {
+            if (skills[i] > skills[maxIndex])
+            {
+                maxIndex = i;
+                win = 0;
+            }
+
+            win++;
+        }
+
+        return maxIndex;
+    }
+
+    public int GetWinner(int[] arr, int k)
+    {
+        int maxIndex = 0, win = 0, lens = arr.Length;
+        for (int i = 1; i < lens && win < k; i++)
+        {
+            if (arr[i] > arr[maxIndex])
+            {
+                maxIndex = i;
+                win = 0;
+            }
+
+            win++;
+        }
+
+        return arr[maxIndex];
+    }
+
+    // }    public static long CountCompleteDayPairs(int[] hours)
+    // {
+    //     long ans = 0, lens = hours.Length;
+    //     Dictionary<int, int> dic = new();
+    //     for (var i = 0; i < lens; i++)
+    //     {
+    //         var temp = (24 - (hours[i]) % 24);
+    //         //单独处理0 12 24
+    //         if (hours[i] % 24 == 0)
+    //         {
+    //             if (dic.TryGetValue(0, out var value))
+    //             {
+    //                 ans += value;
+    //             }
+    //         }
+    //         //如果前面有对应的就加
+    //         else if (dic.TryGetValue(temp, out var value1))
+    //         {
+    //             ans += value1;
+    //         }
+    //         
+    //         //没有就把当前这个加进匹配库
+    //         if (dic.ContainsKey(hours[i] % 24))
+    //         {
+    //             dic[hours[i] % 24]++;
+    //         }
+    //         else
+    //         {
+    //             dic.Add(hours[i] % 24, 1);
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    public static void Main(string[] args)
+    {
+        FindWinningPlayer(new int[] { 7, 11 }, 2);
     }
 }
