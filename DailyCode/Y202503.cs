7d50c3a0598eed3986a4eb81d1591d6d294b5634
humble1324 <799544199@qq.com>
2025-03-11 14:23:23 +0800

    daily update

--- DailyCode/Y202503.cs
+++ DailyCode/Y202503.cs
@@ -1,59 +1,80 @@
 using System;
+using System.Collections.Generic;
 
 namespace LeetCode.DailyCode;
-public class Y202503{
 
-    public int RobNew(int[] nums){
+public class Y202503
+{
+    public int RobNew(int[] nums)
+    {
         int lens = nums.Length;
         // 特殊情况处理：只有一个房子时直接返回
-        if(lens == 1){
+        if (lens == 1)
+        {
             return nums[0];
         }
+
         // 特殊情况处理：只有两个房子时返回较大的值
-        if(lens == 2){
-            return Math.Max(nums[0],nums[1]);
+        if (lens == 2)
+        {
+            return Math.Max(nums[0], nums[1]);
         }
+
         // 创建动态规划数组，dp[i,0]表示不偷第i个房子的最大金额
         // dp[i,1]表示偷第i个房子的最大金额
-        int[,] dp =new int[lens,2];
+        int[,] dp = new int[lens, 2];
         // 初始化：不偷第一个房子
-        dp[0,0] = 0;
+        dp[0, 0] = 0;
         // 初始化：偷第一个房子
-        dp[0,1]=nums[0];
+        dp[0, 1] = nums[0];
         // 动态规划过程
-        for(int i = 1; i < lens; i++) {
+        for (int i = 1; i < lens; i++)
+        {
             // 不偷当前房子时，最大金额为前一个房子偷或不偷的最大值
-            dp[i,0]=Math.Max(dp[i-1,0],dp[i-1,1]);
+            dp[i, 0] = Math.Max(dp[i - 1, 0], dp[i - 1, 1]);
             // 偷当前房子时，只能加上不偷前一个房子的金额
-            dp[i,1]=nums[i]+dp[i-1,0];
+            dp[i, 1] = nums[i] + dp[i - 1, 0];
         }
+
         // 返回最后一个房子偷或不偷的最大值
-        return Math.Max(dp[lens-1,0],dp[lens-1,1]);
+        return Math.Max(dp[lens - 1, 0], dp[lens - 1, 1]);
     }
-}
 
-public class OrderedStream {
+    public static int SumOfBeauties(int[] nums)
+    {
+        //如果前面所有的都小于他,后面所有都大于他 返回2
+        //如果第一条不满足 再判断如果i-1<i<i+1 返回1
+        //都不满足返回0
+        int lens = nums.Length;
+        int[] leftMax = new int[lens];
+        int[] rightMin = new int[lens];
+        leftMax[0] = nums[0];
+        rightMin[lens - 1] = nums[lens - 1];
 
-    private string[] _stream;
-    private int _ptr;
-    public OrderedStream(int n) {
-        _stream = new string[n];
-        _ptr =0;
-    }
-    
-    public IList<string> Insert(int idKey, string value) {
-        _stream[idKey] = value;
-        List<string> ans = new List<string>();
-        
-        while (_ptr < _stream.Length && _stream[_ptr] != null){
-            ans.Add(_stream[_ptr]);
-            _ptr++;
+        int ans = 0;
+        for (var i = 1; i < lens ; i++)
+        {
+            leftMax[i] = Math.Max(leftMax[i - 1], nums[i]);
         }
+
+        for (var i = lens - 2; i >= 0; i--)
+        {
+            
+            rightMin[i] = Math.Min(rightMin[i + 1], nums[i]);
+        }
+
+        for (var i = 1; i < lens - 1; i++)
+        {
+            if (nums[i] > leftMax[i-1] && nums[i] < rightMin[i+1])
+            {
+                ans += 2;
+            }
+            else if (nums[i] > nums[i - 1] && nums[i]< nums[i + 1])
+            {
+                ans++;
+            }
+        }
+
         return ans;
     }
 }
-/**
- * Your OrderedStream object will be instantiated and called as such:
- * OrderedStream obj = new OrderedStream(n);
- * IList<string> param_1 = obj.Insert(idKey,value);
- */
--- Enter.cs
+++ Enter.cs
@@ -7,8 +7,8 @@ public class Enter
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        //Console.WriteLine("Hello World!");
         //Console.WriteLine(Y202501.LargestGoodInteger("1233345678"));
-        //Console.WriteLine(MidSolution.Solution.Trap(new []{0,1,0,2,1,0,1,3,2,1,2,1}));
+        Console.WriteLine(Y202503.SumOfBeauties(new []{5,5,10,4,10}));
     }
 }
